a009500c3bc8904082655a31fc717d94af50ac75
varun-invent <varun.invent@gmail.com>
2018-05-01 12:45:34 +0530

    Added bugs list of ABIDE 2

--- runall.py
+++ runall.py
@@ -2,6 +2,8 @@ import postProcessingFcVolMatchedModularDynamicPipeline as ppfc
 # import preprocessingPipelineImprovedModular as prep
 import preprocessingPipelineImprovedModularWOAnatDynamic as prep
 
+import scoreCorrelation as sc
+
 import hypothesisTest as ht
 import fdrBrainResultsModular as fdres
 import os
@@ -15,11 +17,15 @@ import time
 
 # ------------- Paths ----------------------------------------------------------------------------------------
 
+
 path_cwd = os.getcwd()
 path_split_list = path_cwd.split('/')
 s = path_split_list[0:-1] # for getting to the parent dir of pwd
 s = opj('/',*s) # *s converts list to path, # very important to add '/' in the begining so it is read as directory later
 
+# New path to store results:
+
+results_path = '/mnt/project1/home1/varunk/results/'
 
 
 json_path = 'scripts/json/paths.json'
@@ -28,7 +34,7 @@ with open(json_path, 'rt') as fp:
 
 
 
-base_directory = opj(s,task_info["base_directory_for_results"])
+base_directory = opj(results_path,task_info["base_directory_for_results"])
 motion_correction_bet_directory = task_info["motion_correction_bet_directory"]
 parent_wf_directory = task_info["parent_wf_directory"]
 functional_connectivity_directory = task_info["functional_connectivity_directory"]
@@ -69,6 +75,9 @@ categoryInfo = None
 
 current_time = time.asctime( time.localtime(time.time()) )
 
+if not os.path.exists(base_directory):
+    os.makedirs(base_directory)
+
 log_path = opj(base_directory,"log.txt")
 log = open(log_path, 'a')
 print("-------------Starting the analysis at %s--------------\n"%(current_time))
@@ -101,9 +110,25 @@ layout = BIDSLayout(data_directory)
 if number_of_subjects == -1:
     number_of_subjects = len(layout.get_subjects())
 
+# ABIDE II Bugs
+
+bugs_abide2 = ['28093', '28093', '28681',  '28682', '28683',  '28687', '28711', '28712', '28713', '28741', '28745',  '28751', '28755', '28756', '28757', '28758',
+'28759', '28761', '28762','28763', '28764','28765','28766','28767','28768','28769','28770','28771','28772','28773','28774','28775','28776','28777','28778','28779',
+'28780','28781','28782','28783'
+]
+
+bugs = bugs_abide2
 
 subject_list = (layout.get_subjects())[0:number_of_subjects]
 # subject_list = list(map(int, subject_list))
+
+# Ignore Bugs
+subject_list = list(set(subject_list) - set(bugs))
+
+subject_list.sort()
+
+
+
 
 # -----------------------------------File List----------------------------------
 # group1FilesPath = ''
@@ -144,18 +169,19 @@ fdr_results_dir,
 score_corr_dir
 ]
 
-PREPROC = 0
-POSTPROC = 0
+PREPROC = 1
+POSTPROC = 1
 HYPOTEST = 0
 FDRES = 0
-SCORE_CORR = 1
+SCORE_CORR = 0
 
-match = 1 # Age matching
+match = 0 # Age matching
 applyFisher = True
 
 # itr = (list(itertools.product([0, 1], repeat=3)))
-itr = [(1,0,1,1,1)]
 # itr = [(1,1,1,1,1)]
+# itr = [(1,0,1,1,1)]
+itr = [(0,0,1,1,0)]
 
 log.write("Operations:\n")
 log.write("Preprocess = %s\n"%(PREPROC))
@@ -191,10 +217,17 @@ if PREPROC == 1:
 # ------------------------PostProcess------------------------------------------
 if POSTPROC == 1:
     print('PostProcessing')
-
-
+    log.write("Postprocessing Params\n")
 
     for motion_param_regression, global_signal_regression, smoothing, band_pass_filtering, volCorrect in itr:
+        log.write("motion_param_regression  = %s\n"%(motion_param_regression))
+        log.write("global_signal_regression  = %s\n"%(global_signal_regression))
+        log.write("smoothing  = %s\n"%(smoothing))
+        log.write("band_pass_filtering  = %s\n"%(band_pass_filtering))
+        log.write("volCorrect  = %s\n"%(volCorrect))
+
+
+
         combination = 'motionRegress' + str(int(motion_param_regression)) + \
          'global' + str(int(global_signal_regression)) + 'smoothing' + str(int(smoothing)) +\
          'filt' + str(int(band_pass_filtering))
@@ -207,7 +240,7 @@ if POSTPROC == 1:
 
 
 # ------------------- Hypothesis Test ------------------------------------------
-
+# ABIDE1 BUGS
 bugs = ['51232','51233','51242','51243','51244','51245','51246','51247','51270','51310','50045', '51276', '50746', '50727', '51276']
 
 if HYPOTEST == 1:
@@ -247,5 +280,5 @@ if SCORE_CORR == 1:
     # bugs = []
 
     for motion_param_regression, global_signal_regression, smoothing,band_pass_filtering, volCorrect in itr:
-        ht.main(paths, bugs,applyFisher,categoryInfo, match, motion_param_regression, global_signal_regression, band_pass_filtering, \
+        sc.main(paths, bugs,applyFisher,categoryInfo, match, motion_param_regression, global_signal_regression, band_pass_filtering, \
             smoothing, num_proc)
